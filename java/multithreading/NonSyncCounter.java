d6f701776f8df379a9dfba6f1c47f667cef3f38b
bob2build <bob2build.2020@gmail.com>
2016-04-16 02:51:59 +0530

    Add multithreading sample

---
+++ java/multithreading/NonSyncCounter.java
@@ -0,0 +1,45 @@
+public class NonSyncCounter{
+  public static void main(String[] args) throws Exception{
+    Counter counter = new Counter();
+    Thread t1 = new Thread(new CounterTask(counter));
+    Thread t2 = new Thread(new CounterTask(counter));
+    Thread t3 = new Thread(new CounterTask(counter));
+    t1.start();
+    t2.start();
+    t3.start();
+    t1.join();
+    t2.join();
+    t3.join();
+    System.out.println(counter);
+  }
+}
+
+class CounterTask implements Runnable{
+  private Counter counter;
+  public CounterTask(Counter counter){
+    this.counter = counter;
+  }
+
+  public void run(){
+    for(int i = 0;i<10;i++){
+      System.out.println("Increment : " + Thread.currentThread().getId());
+      counter.increment();
+    }
+  }
+}
+
+class Counter {
+  int count;
+  public synchronized void increment(){
+    int t = count;
+    try{
+      Thread.sleep(100);
+    } catch(InterruptedException e){
+      System.err.println("Error while trying to sleep");
+    }
+    count = t + 1;
+  }
+  public String toString(){
+    return String.valueOf(count);
+  }
+}
--- java/overview.md
+++ java/overview.md
@@ -1,6 +1,17 @@
 # Java Tutorial
 ## Overview
+* Java vs Other languages
 * Java Programming language
 * Java Development Environment (JDK)
 * Java Runtime Environment (JRE)
 ## Concepts
+### OOPS
+http://www.javatpoint.com/java-oops-concepts
+### Collections
+https://docs.oracle.com/javase/tutorial/collections/intro/
+https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html
+** Sort Numbers
+** Sort Strings
+** Sort Names
+* Multithreading
+* Classloader
